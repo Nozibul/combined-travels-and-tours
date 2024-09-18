db90499d1cb22beb6762afa40be9eadc53c2f6df
Nozibul <nozibulislamspi@gmail.com>
2024-09-18 12:44:52 +0600

    Adding footer subsection

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,27 @@
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from '@/components/navbar/Navbar';
-import Footer from '@/components/footer/Footer';
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({
-     subsets: ["latin"],
-     display: "swap",
-     variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
 export const metadata = {
-     title: "Combined Tours & Travels",
-     description: "Combined Tours & Travels web application",
+  title: "Combined Tours & Travels",
+  description: "Combined Tours & Travels web application",
 };
 
 export default function RootLayout({ children }) {
-     return (
-          <html lang="en">
-               <body className={inter.variable}>
-                    <Navbar />
-                    <main className="">{children}</main>
-                    <Footer />
-               </body>
-          </html>
-     );
+  return (
+    <html lang="en">
+      <body className={inter.variable}>
+        <Navbar />
+        <main className="">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-
+import { TextTitle } from "../text/textTitle/TextTitle";
+import styles from "./Footer.module.css";
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  )
-}
+    <>
+      <footer className={styles.footer}>
+        <div className={styles["footer-content"]}>
+          <h2>Footer</h2>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="p-4 border">
+              <div className="underline-element">
+                <TextTitle textTitle="Find Us " />
+              </div>
+            </div>
+            <div className="p-4 border ">
+              <div className="underline-element">
+                <TextTitle textTitle="Call Us" />
+              </div>
+            </div>
+            <div className="p-4 border ">
+              <div className="underline-element">
+                <TextTitle textTitle="Mail Us" />
+              </div>
+            </div>
+          </div>{" "}
+          <br />
+          <div className="p-2 border-2">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+              <div className="p-4 border">Section 1</div>
+              <div className="p-4 border">Section 2</div>
+              <div className="p-4 border">Section 3</div>
+              <div className="p-4 border">Section 4</div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
