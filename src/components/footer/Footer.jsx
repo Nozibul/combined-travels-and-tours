b4a14071b4f0634d8f78541cc444af96bbac11c6
Nozibul <nozibulislamspi@gmail.com>
2024-09-24 12:37:52 +0600

    Footer 1st section completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,30 +1,48 @@
 import { TextTitle } from "../text/textTitle/TextTitle";
+import contactData from "../../../local-json/footer"; // Import the JSON data
+import { FaLocationDot, FaPhone } from "react-icons/fa6";
+import { IoIosMailOpen } from "react-icons/io";
 import styles from "./Footer.module.css";
+
+const iconMap = {
+  FaLocationDot,
+  FaPhone,
+  IoIosMailOpen,
+};
+
 const Footer = () => {
   return (
     <>
       <footer className={styles.footer}>
         <div className={styles["footer-content"]}>
-          <h2>Footer</h2>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-            <div className="p-4 border">
-              <div className="underline-element">
-                <TextTitle textTitle="Find Us " />
-              </div>
-            </div>
-            <div className="p-4 border ">
-              <div className="underline-element">
-                <TextTitle textTitle="Call Us" />
-              </div>
-            </div>
-            <div className="p-4 border ">
-              <div className="underline-element">
-                <TextTitle textTitle="Mail Us" />
-              </div>
-            </div>
-          </div>{" "}
-          <br />
-          <div className="p-2 border-2">
+          <div className="mb-8 grid grid-cols-1 gap-[3rem] sm:grid-cols-2 lg:grid-cols-3">
+            {contactData?.map((item, index) => {
+              const IconComponent = iconMap[item.icon];
+              return (
+                <div className="" key={index}>
+                  <div className={styles.container}>
+                    <div className={styles.leftBox}>
+                      <div className={styles.boxNumber}>
+                        {IconComponent && <IconComponent />}
+                      </div>
+                      <div
+                        className={`${styles.boxTitle} mt-2 underline-element`}
+                      >
+                        <TextTitle textTitle={item.title} titlePosition />
+                      </div>
+                    </div>
+                    <div className={styles.bottomBox}>
+                      {item.details.map((detail, idx) => (
+                        <span key={idx}>{detail}</span>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <hr />
+          <div className="p-2 mt-8 border-2">
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
               <div className="p-4 border">Section 1</div>
               <div className="p-4 border">Section 2</div>
